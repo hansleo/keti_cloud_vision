4da44b9d682cd414b6eb267d6fe6b4ab46533122
Handong LEE <hansleo3589@gmail.com>
2017-01-06 15:18:31 +0900

    Update GVC_text_test.py

--- test/GVC_text_test.py
+++ test/GVC_text_test.py
@@ -1,27 +1,24 @@
+#Author: Lee, HANDONG(hansleo)
+
 import sys
 import os
 import textindex
 
-sys.path.insert(0, '/home/leo/cloud-vision/python/text')
-img_dir = '/home/leo/cloud-vision/pics/'
+sys.path.insert(0, '/directory/of/cloud-vision/python/text')
+img_dir = '/directory/of/images/'
 
 def find_images(img_dir):
 	imgs= list()
-
 	for root, dirs, files in os.walk(img_dir):
 		for img in files:
 			imgs.append(img_dir + img)
-
 	return imgs
-
 
 imgs = find_images(img_dir)
 vision_api = textindex.VisionApi()
-
 result = vision_api.detect_text(imgs)
-
 for img in imgs:
-	print('\nimage name: ', img)
+	print('\nimage: ', img)
 	for img_idx in range(1, len(result[img])):
 		print('\t', result[img][img_idx]['description'])
 
